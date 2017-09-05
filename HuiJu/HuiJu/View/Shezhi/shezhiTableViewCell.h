7d9c491ac5d7f4825f915665f0767267fb9d603f
HZX <1660896068@qq.com>
2017-09-05 09:26:03 +0800

    cell

---
+++ HuiJu/HuiJu/View/Shezhi/shezhiTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  shezhiTableViewCell.h
+//  HuiJu
+//
+//  Created by admin1 on 2017/9/4.
+//  Copyright © 2017年 shiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface shezhiTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *biaoti;
+@property (weak, nonatomic) IBOutlet UILabel *neirong;
+
+
+
+@end
---
+++ HuiJu/HuiJu/View/Shezhi/shezhiTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  shezhiTableViewCell.m
+//  HuiJu
+//
+//  Created by admin1 on 2017/9/4.
+//  Copyright © 2017年 shiki. All rights reserved.
+//
+
+#import "shezhiTableViewCell.h"
+
+@implementation shezhiTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
