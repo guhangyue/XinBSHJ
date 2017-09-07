0ab76b8a40bc7494c5541edc1a5eea30a4362c7f
HZX <1660896068@qq.com>
2017-09-07 16:12:21 +0800

    ...

---
+++ HuiJu/HuiJu/View/ZhiFu/zhifuTableViewController.m
@@ -0,0 +1,223 @@
+//
+//  zhifuTableViewController.m
+//  HuiJu
+//
+//  Created by admin1 on 2017/9/5.
+//  Copyright © 2017年 shiki. All rights reserved.
+//
+
+#import "zhifuTableViewController.h"
+
+@interface zhifuTableViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *tiyankaName;
+@property (weak, nonatomic) IBOutlet UILabel *tiyankaguishu;
+@property (weak, nonatomic) IBOutlet UILabel *danjia;
+@property (weak, nonatomic) IBOutlet UILabel *shuliang;
+@property (weak, nonatomic) IBOutlet UILabel *jiage;
+- (IBAction)jiajian:(UIStepper *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UITableView *tablecell;
+
+@property (strong,nonatomic)NSArray *zhifuArr;
+
+@end
+
+@implementation zhifuTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//设置导航栏样式
+-(void)setNavigationItem{
+    self.navigationItem.title = @"支付";
+    //设置导航条的颜色（风格颜色）
+    self.navigationController.navigationBar.barTintColor = UIColorFromRGB(0, 100, 255);
+    //实例化一个button
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    //设置button的位置大小
+    leftBtn.frame = CGRectMake(0, 0, 20, 20);
+    //设置背景图片
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回"] forState:UIControlStateNormal];
+    //给按钮添加事件
+    [leftBtn addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftBtn];
+}
+//用model的方式返回上一页
+- (void)backAction{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    //[self.navigationController popViewControllerAnimated:YES];//用push返回上一页
+}
+
+-(void)uiLayout{
+    //_hotelNameLabel.text= _hotelModel.hotelName;
+    //_startTimeLabel.tag= _hotelModel.startTime;
+    // _endTimeLabel.tag=_hotelModel.endTime;
+    // _payLabel.text=[NSString stringWithFormat:@"%@元",_hotelModel.hotelMoney];
+    self.tableView.tableFooterView =[UIView new];
+    //将表格视图设置为"编辑中"
+    self.tableView.editing = YES;
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    //用代码来选中表格视图中某个细胞
+    [self.tableView selectRowAtIndexPath:indexPath animated:YES scrollPosition:UITableViewScrollPositionNone];
+}
+
+-(void)dataInitialize{
+    _zhifuArr=@[@"支付宝支付",@"微信支付",@"银联支付"];
+}
+
+-(void)payAction{
+    switch(self.tableView.indexPathForSelectedRow.row){
+        case 0:{
+            NSString *tradeNo=[GBAlipayManager generateTradeNO];
+            //[GBAlipayManager alipayWithProductName:_detail.eName amount:_detail.currentPrice tradeNO:tradeNo notifyURL:nil productDescription:[NSString stringWithFormat:@"%@的活动报名费",_detail.eName] itBPay:@"30"];
+        }
+            break;
+        case 1:{
+            
+        }
+            break;
+        case 2:{
+            
+        }
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)setFootViewForTableView{
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, UI_SCREEN_W, 45)];
+    
+    UIButton *exitBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    exitBtn.frame = CGRectMake(0, 5, UI_SCREEN_W, 40.f);
+    [exitBtn setTitle:@"确定支付" forState:UIControlStateNormal];
+    //设置按钮标题的字体大小
+    exitBtn.titleLabel.font = [UIFont boldSystemFontOfSize:15.f];
+    [exitBtn setTitleColor:UIColorFromRGB(221.f, 129.f, 116.f) forState:UIControlStateNormal];
+    exitBtn.backgroundColor = [UIColor whiteColor];
+    [exitBtn addTarget:self action:@selector(exitAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [view addSubview:exitBtn];
+    
+    [_tablecell setTableFooterView:view];
+}
+
+//按钮的点击事件
+- (void)exitAction: (UIButton *)button{
+    //NSLog(@"%@", @"退出");
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"是否要支付？" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *actionA = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        //        [self exit];
+    }];
+    UIAlertAction *actionB = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:actionA];
+    [alert addAction:actionB];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return _zhifuArr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PayCell" forIndexPath:indexPath];
+    cell.textLabel.text=_zhifuArr[indexPath.row];
+    // Configure the cell...
+    
+    return cell;
+}
+
+//设置cell的高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50.f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //遍历表格视图中所有选中状态下的细胞
+    for (NSIndexPath*eachIP in tableView.indexPathsForSelectedRows) {
+        //当选中的细胞不是当前正在按的这个细胞的情况下
+        if (eachIP !=indexPath) {
+            //将细胞从选中状态改为不选中状态
+            [tableView deselectRowAtIndexPath:eachIP animated:YES];
+        }
+    }
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)jiajian:(UIStepper *)sender forEvent:(UIEvent *)event {
+}
+@end
