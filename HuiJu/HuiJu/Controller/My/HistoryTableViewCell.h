e720651a77b0a19d4c9cc3918d1325c9e46f9b79
辜航跃 <1367911785@qq.com>
2017-09-16 09:46:49 +0800

    添加cell、

---
+++ HuiJu/HuiJu/Controller/My/HistoryTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  HistoryTableViewCell.h
+//  HuiJu
+//
+//  Created by admin1 on 2017/9/15.
+//  Copyright © 2017年 shiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *dingdanLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tiYanLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dianMingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *moneyLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ HuiJu/HuiJu/Controller/My/HistoryTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.m
+//  HuiJu
+//
+//  Created by admin1 on 2017/9/15.
+//  Copyright © 2017年 shiki. All rights reserved.
+//
+
+#import "HistoryTableViewCell.h"
+
+@implementation HistoryTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
